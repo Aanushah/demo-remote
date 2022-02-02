a51693ee68127bbbebca52e053b47d5eb5ed226f
Aanushah <98877000+Aanushah@users.noreply.github.com>
2022-02-02 22:10:26 +0530

    initial commit

---
+++ math.py
@@ -0,0 +1,15 @@
+#Addition of line
+def add(x,y):
+    return x+y
+    
+#Subtract implementation
+def subtract(x,y):
+    return x-y
+ 
+#multiply implementation 
+def multiply(x,y):
+    pass
+    
+#Divide Implementaion
+del divide(x,y):
+    pass
